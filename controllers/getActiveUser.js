c72722853816a5f8b5856bcf9a99e8cbeca30838
Aleksey Alekseev <AlekseevFr@yandex.ru>
2023-02-16 23:46:29 +0300

    Финальные правки перед Ревью

--- controllers/getActiveUser.js
+++ controllers/getActiveUser.js
@@ -2,20 +2,15 @@ const { constants } = require('http2');
 const User = require('../models/user');
 
 async function getActiveUser(req, res) {
-  console.log('getActive', req);
-
   try {
     const userId = req.params.userId || req.user._id;
     const user = await User.findById(userId);
-    console.log('user', req.user, user);
-
     if (!user) {
       return res.status(constants.HTTP_STATUS_NOT_FOUND).send({ message: 'Пользователь не найден' });
     }
 
     return res.send(user);
   } catch (err) {
-    console.log('err', err);
     return res.status(constants.INTERNAL_ERROR).send({ message: 'Произошла ошибка' });
   }
 }
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,8 +6,6 @@ module.exports = (req, res, next) => {
     const { authorization } = req.headers;
 
     if (!authorization || !authorization.startsWith('Bearer ')) {
-      // const err = new UnAuthorized('Необходима авторизация');
-      // next(err);
       throw new UnAuthorized('Необходима авторизация');
     }
     let payload;
@@ -16,13 +14,8 @@ module.exports = (req, res, next) => {
     try {
       payload = jwt.verify(token, 'super-strong-secret');
     } catch (err) {
-      // const error = new UnAuthorized(
-      //   'Необходима авторизация',
-      // );
-      // next(error);
       throw new UnAuthorized('Необходима авторизация');
     }
-    console.log('payload', payload, token, authorization);
     req.user = payload;
     next();
   } catch (err) {
